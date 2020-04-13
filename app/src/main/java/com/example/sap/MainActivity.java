71eb08b3b97b83d1177453cd0adf6bcb03ac3095
peterburgs <peterburgs.vn@gmail.com>
2020-04-13 12:05:10 +0700

    Update April 13, 2020

--- app/src/main/java/com/example/sap/MainActivity.java
+++ app/src/main/java/com/example/sap/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.sap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        ListView myListView=findViewById(R.id.myListView);
+
+        final ArrayList<String> myFamily=new ArrayList<String>();
+
+        myFamily.add("Peter Burgs");
+        myFamily.add("John Wick");
+        myFamily.add("Donald Trumpet");
+        myFamily.add("Back Obama");
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, myFamily);
+        myListView.setAdapter(arrayAdapter);
+
+
+        //Show the name of the item clicked
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You clicked: "+myFamily.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

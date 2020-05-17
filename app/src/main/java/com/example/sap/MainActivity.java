3e7ea37afd660aa25ead58d139ed0ebb50cac1c3
Le Duc Thinh <38351261+ducthinh2111@users.noreply.github.com>
2020-05-17 08:46:06 +0700

    amplify add auth, amplify add api
    Add authentication and API services to app

--- app/src/main/java/com/example/sap/MainActivity.java
+++ app/src/main/java/com/example/sap/MainActivity.java
@@ -17,26 +17,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
-        ListView myListView=findViewById(R.id.myListView);
-
-        final ArrayList<String> myFamily=new ArrayList<String>();
-
-        myFamily.add("Peter Burgs");
-        myFamily.add("John Wick");
-        myFamily.add("Donald Trumpet");
-        myFamily.add("Back Obama");
-
-        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, myFamily);
-        myListView.setAdapter(arrayAdapter);
-
-        //Don't know how to use git
-        //Show the name of the item clicked
-        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "You clicked: "+myFamily.get(position), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }

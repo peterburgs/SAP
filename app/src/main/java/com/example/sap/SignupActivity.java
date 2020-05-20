b8b181a1d579275da599361d65a3ef3972078725
peterburgs <peterburgs.vn@gmail.com>
2020-05-20 21:29:50 +0700

    Modify SignUp Activity & Add ConfirmSignUp Activity

--- app/src/main/java/com/example/sap/SignupActivity.java
+++ app/src/main/java/com/example/sap/SignupActivity.java
@@ -5,25 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
     TextView tv_signin;
+    Button btn_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
+        //Define tags from XML to Java
         tv_signin = findViewById(R.id.tv_signin);
+        btn_signup = findViewById(R.id.btn_signup);
 
-        //Touch "Sign in" to go back to "LogIn Activity"
+
+        //User Touches "Sign in" to go back to "LogIn Activity"
         tv_signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
                 startActivity(intent);
 
+            }
+        });
+
+        //User fills out the form and touch "Sign Up"
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, ConfirmSignup.class);
+                startActivity(intent);
             }
         });
     }

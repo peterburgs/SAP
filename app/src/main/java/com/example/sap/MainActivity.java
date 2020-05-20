87be4aa2c17156e7b1765489e5e10ea05f966580
peterburgs <peterburgs.vn@gmail.com>
2020-05-20 18:12:44 +0700

    Add SignUp Activity. Add ID for tags. Switch among activities.

--- app/src/main/java/com/example/sap/MainActivity.java
+++ app/src/main/java/com/example/sap/MainActivity.java
@@ -3,18 +3,11 @@ package com.example.sap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,13 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         btn_login = findViewById(R.id.btn_login);
         tv_signup = findViewById(R.id.tv_signup);
 
+        //User touches Login Button
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                //Hello World
+            }
+        });
+
+
+        //User touches Signup Text view
+        tv_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/sap/SignupActivity.java
+++ app/src/main/java/com/example/sap/SignupActivity.java
@@ -7,22 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
-
-
-    TextView tv_signup;
+public class SignupActivity extends AppCompatActivity {
+    TextView tv_signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        tv_signup = findViewById(R.id.tv_signup);
+        setContentView(R.layout.activity_signup);
 
-        tv_signup.setOnClickListener(new View.OnClickListener() {
+        tv_signin = findViewById(R.id.tv_signin);
+
+        //Touch "Sign in" to go back to "LogIn Activity"
+        tv_signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
                 startActivity(intent);
+
             }
         });
     }

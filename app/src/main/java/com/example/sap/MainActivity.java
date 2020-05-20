d996880434352a117191ad54a800bf97a1cdc374
peterburgs <peterburgs.vn@gmail.com>
2020-05-20 08:53:05 +0700

    Update Welcome Activity.

--- app/src/main/java/com/example/sap/MainActivity.java
+++ app/src/main/java/com/example/sap/MainActivity.java
@@ -2,25 +2,39 @@ package com.example.sap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_login;
+    TextView tv_signup;
+    Toast toastMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = findViewById(R.id.btn_login);
+        tv_signup = findViewById(R.id.tv_signup);
 
-
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

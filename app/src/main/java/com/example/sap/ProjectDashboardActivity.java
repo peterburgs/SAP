87fac09d620b22a4b2467659f9aa7f52f93e3ec3
peterburgs <peterburgs.vn@gmail.com>
2020-06-02 22:29:32 +0700

    Handle Button Create New Project
    no backend code yet!

--- app/src/main/java/com/example/sap/ProjectDashboardActivity.java
+++ app/src/main/java/com/example/sap/ProjectDashboardActivity.java
@@ -1,10 +1,19 @@
 package com.example.sap;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class ProjectDashboardActivity extends AppCompatActivity {
 
@@ -19,24 +28,26 @@ public class ProjectDashboardActivity extends AppCompatActivity {
     int img[] = {R.drawable.project_img1, R.drawable.project_img2, R.drawable.project_img3, R.drawable.project_img4, R.drawable.project_img5};
 
     RecyclerView rcvProjectList;
+    MaterialToolbar topAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_dashboard);
         rcvProjectList = findViewById(R.id.rcvProjectList);
+        topAppBar = findViewById(R.id.topAppBar);
 
+        topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                //todo: Create New Project Here
 
+                return true;
+            }
+        });
         ProjectListAdapter projectListAdapter = new ProjectListAdapter(this, s1, s2, img);
         rcvProjectList.setAdapter(projectListAdapter);
         rcvProjectList.setLayoutManager(new LinearLayoutManager(this));
 
     }
-
-    //Add new Project
-    private void addProject() {
-
-    }
-
-
 }

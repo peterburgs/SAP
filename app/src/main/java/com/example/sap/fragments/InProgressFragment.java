72fb149aecf25c22dab111394bee9c9d56a200ba
peterburgs <peterburgs.vn@gmail.com>
2020-06-04 23:10:20 +0700

    Design: InProgress Fragment & Java code

--- app/src/main/java/com/example/sap/fragments/InProgressFragment.java
+++ app/src/main/java/com/example/sap/fragments/InProgressFragment.java
@@ -2,13 +2,19 @@ package com.example.sap.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.sap.R;
+import com.example.sap.adapters.InProgressAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +31,15 @@ public class InProgressFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    RecyclerView rcvInProgress;
 
+    //Hardcoded Data:
+    String taskName[] = {"SAP-1", "SAP-2", "SAP-3", "SAP-4", "SAP-1", "SAP-2", "SAP-3", "SAP-4"};
+    String taskSummary[] = {"Design raw UI", "Apply Material Design Component", "Add Constraints", "Beautify", "Design raw UI", "Apply Material Design Component", "Add Constraints", "Beautify"};
+    String taskLabel[] = {"Design UI", "Design UI", "Design UI", "Design UI", "Design UI", "Design UI", "Design UI", "Design UI"};
+    String assignee[] = {"peterburgs", "peterburgs", "peterburgs", "starea", "peterburgs", "peterburgs", "peterburgs", "starea"};
+
+    //
     public InProgressFragment() {
         // Required empty public constructor
     }
@@ -55,6 +69,24 @@ public class InProgressFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rcvInProgress = getView().findViewById(R.id.rcvInProgress);
+        InProgressAdapter inProgressAdapter=new InProgressAdapter(getContext(), taskName, taskSummary, taskLabel, assignee);
+
+        rcvInProgress.setAdapter(inProgressAdapter);
+        rcvInProgress.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        inProgressAdapter.setOnItemClickListener(new InProgressAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                //todo: handle Nav to EditTask
+                Toast.makeText(getContext(), "Task Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override

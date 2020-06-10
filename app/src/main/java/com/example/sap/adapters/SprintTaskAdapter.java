8d7cf803f21bf1728f024ac1f9ab651c85e88436
Le Duc Thinh <38351261+ducthinh2111@users.noreply.github.com>
2020-06-10 15:14:03 +0700

    add sprint task adapter

---
+++ app/src/main/java/com/example/sap/adapters/SprintTaskAdapter.java
@@ -0,0 +1,88 @@
+package com.example.sap.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amplifyframework.datastore.generated.model.Task;
+import com.example.sap.R;
+
+import java.util.ArrayList;
+
+public class SprintTaskAdapter extends RecyclerView.Adapter<SprintTaskAdapter.ViewHolder> {
+    private ArrayList<Task> taskList;
+    private Context context;
+    private ToDoAdapter.OnItemClickListener mListener;
+
+    //Interface to Handle Clicking a specific item
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(ToDoAdapter.OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    //Constructor
+    public SprintTaskAdapter(Context context, ArrayList<Task> taskList) {
+        this.context = context;
+        this.taskList = taskList;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.sprint_task_row, parent, false);
+
+        return new ViewHolder(view, mListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvTaskName.setText(taskList.get(position).getName());
+        holder.tvTaskSummary.setText(taskList.get(position).getSummary());
+        holder.tvTaskLabel.setText(taskList.get(position).getLabel());
+        holder.tvAssignee.setText(taskList.get(position).getAssignee().getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTaskName, tvTaskSummary, tvTaskLabel, tvAssignee;
+        protected CardView cvSprintTask;
+
+        public ViewHolder(@NonNull View itemView, ToDoAdapter.OnItemClickListener listener) {
+            super(itemView);
+
+            tvTaskName = itemView.findViewById(R.id.tvTaskName);
+            tvTaskSummary = itemView.findViewById(R.id.tvTaskSummary);
+            tvTaskLabel = itemView.findViewById(R.id.tvTaskLabel);
+            tvAssignee = itemView.findViewById(R.id.tvAssignee);
+            cvSprintTask = itemView.findViewById(R.id.cvSprintTask);
+
+            cvSprintTask.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}

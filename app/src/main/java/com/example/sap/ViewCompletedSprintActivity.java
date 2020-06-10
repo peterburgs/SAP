d15945334b087ff0d48b472c3be942c940994ccc
peterburgs <peterburgs.vn@gmail.com>
2020-06-10 14:38:53 +0700

    Design UI: Add Activities for editting sprint

---
+++ app/src/main/java/com/example/sap/ViewCompletedSprintActivity.java
@@ -0,0 +1,14 @@
+package com.example.sap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewCompletedSprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_completed_sprint);
+    }
+}
